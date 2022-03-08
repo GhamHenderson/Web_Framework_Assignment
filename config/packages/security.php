616e445a32afa25a116846fd7572e018e02bc400
Graham Henderson <62728935+GhamHenderson@users.noreply.github.com>
2022-03-08 13:28:25 +0000

    Remember me Option

---
+++ config/packages/security.php
@@ -0,0 +1,14 @@
+<?php
+
+// config/packages/security.php
+use Symfony\Config\SecurityConfig;
+
+return static function (SecurityConfig $security) {
+
+$security->firewall('main')
+
+->rememberMe()
+->secret('%kernel.secret%')
+
+->alwaysRememberMe(true);
+};
