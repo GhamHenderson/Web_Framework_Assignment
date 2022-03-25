2661177a156f249c1e168d9721167faf56c6b999
GhamHenderson <B00130193@mytudublin.ie>
2022-03-25 17:37:09 +0000

    Added Role Ifs for Chef

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,30 +3,82 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Repository\OrderItemRepository;
+use App\Form\Product1Type;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ProductController extends AbstractController
 {
+    #[Route('/product', name: 'product_index', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/product/new', name: 'product_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(Product1Type::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/product/{id}/edit', name: 'product_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(Product1Type::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'product_delete', methods: ['POST'])]
+    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
     #[Route('/product/{id}', name: 'product.detail')]
-    public function index(Product $product): Response
+    public function detailindex(Product $product): Response
     {
         return $this->render('product/detail.html.twig', [
             'product' => $product,
         ]);
-    }
-
-    #[Route('/product', name: 'menu')]
-    public function aboutus(ProductRepository $productRepository): Response
-    {
-        $template = 'product/product.html.twig';
-        $argsArray = ['products' => $productRepository->findAll()];
-
-        return $this->render($template, $argsArray);
     }
 
     #[Route('/checkout', name: 'checkout')]
@@ -39,11 +91,10 @@ class ProductController extends AbstractController
     }
 
     #[Route('/basket', name: 'basket')]
-    public function basket(OrderRepository $orderRepository): Response
+    public function basket(): Response
     {
-
         $template = 'product/basket.html.twig';
-        $argsArray = ['basketproducts' => $orderRepository->findall()];
+        $argsArray = [];
         return $this->render($template, $argsArray);
     }
 }
