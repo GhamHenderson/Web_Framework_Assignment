725f3131181ac67ee4b9a167ff13da84cfbdc1b1
GhamHenderson <B00130193@mytudublin.ie>
2022-04-26 15:00:33 +0100

    Fixed Issue With DateTime and Fixed Upload Feature

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use App\Entity\Product;
 use App\Form\Product1Type;
-use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Gedmo\Sluggable\Util\Urlizer;
 
 class ProductController extends AbstractController
 {
@@ -26,13 +25,30 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/new', name: 'product_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, LoggerInterface $logger): Response
     {
         $product = new Product();
         $form = $this->createForm(Product1Type::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form['imageFilename']->getData();
+            $logger->info("1- FILE = $uploadedFile");
+            if ($uploadedFile != null) {
+                $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/menuImages';
+                $logger->info("2- dir = $destination");
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $logger->info("3- dir = $originalFilename");
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+                $logger->info("4- $newFilename");
+                $uploadedFile->move(
+                    $destination,
+                    $newFilename
+                );
+                $logger->info("5 - $uploadedFile");
+                $product->setImageFilename("uploads/menuImages/" . $newFilename);
+            }
+
             $entityManager->persist($product);
             $entityManager->flush();
 
@@ -46,27 +62,31 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}/edit', name: 'product_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    public function edit(LoggerInterface $logger, Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
+
         $form = $this->createForm(Product1Type::class, $product);
-
-        //$uploadedFile = $request->files->get('image');
-        $uploadedFile = $form['imageFilename']->getData();
-        if ($uploadedFile != null) {
-            $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/menuImages';
-            $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-            $newFilename = $originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
-            $uploadedFile->move(
-                $destination,
-                $newFilename
-            );
-
-            $product->setImageFilename($newFilename);
-        }
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form['imageFilename']->getData();
+            $logger->info("1- FILE = $uploadedFile");
+            if ($uploadedFile != null) {
+                $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/menuImages';
+                $logger->info("2- dir = $destination");
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $logger->info("3- dir = $originalFilename");
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+                $logger->info("4- $newFilename");
+                $uploadedFile->move(
+                    $destination,
+                    $newFilename
+                );
+                $logger->info("5 - $uploadedFile");
+
+                $product->setImageFilename("uploads/menuImages/" . $newFilename);
+            }
+
             $entityManager->flush();
             return $this->redirectToRoute('product_index', [], Response::HTTP_SEE_OTHER);
         }
